fc0b0edd30cb041c7eb8c2136246d03f896d6b2d
dstntLghts <ctzeranis@gmail.com>
2021-03-07 00:02:54 +0200

    week 2 assignment 1

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -70,14 +70,6 @@ function RenderDish({dish}) {
         return (
             <div className="container">
                 <div className="row">
-                    {/* <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>  */}
                     <RenderDish dish={props.dish} />
                     <RenderComments dish={props.comments} />               
                 </div>
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -48,6 +49,7 @@ class Main extends Component {
         <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
             <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
